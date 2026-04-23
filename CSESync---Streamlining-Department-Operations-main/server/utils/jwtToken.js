c670f72fc738aea7d0d44263cf1c85a76d17a386
RajasiDeshpande <161115992+RajasiDeshpande@users.noreply.github.com>
2026-04-23 19:36:15 +0530

    commit

--- CSESync---Streamlining-Department-Operations-main/server/utils/jwtToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const signToken = (id, role) => {
-  return jwt.sign({ id, role }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE || '30d',
-  });
-};
-
-module.exports = { signToken };
